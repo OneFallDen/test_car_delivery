f0be89292ecf731e5511668cacc753b6bfc3bc4d
OneFallDen <onefallden@gmail.com>
2023-05-24 19:03:15 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="us_delivery",
+    version="1.0.0",
+    url="https://github.com/OneFallDen/test_car_delivery",
+    packages=find_packages()
+)
